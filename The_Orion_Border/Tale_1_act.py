1c3bdedc1c909cd280ddc62a7655df1f420425f8
Pretherox <62885155+Pretherox@users.noreply.github.com>
2023-10-09 16:57:30 +0200

    Add files via upload

---
+++ The_Orion_Border/Tale_1_act.py
@@ -0,0 +1,11 @@
+from Ship import Resources
+from Ship import ship
+from act_1_events import mission_traveler
+#=================Intro=================
+
+print('Like many others, a new crew has formed to try reach the deepest parts of the Galaxy, The Orion Border.')
+print('It\'s a dangeroys travel, where many had died traying. Make sure to have enough fuel and food for it.')
+print('\n')
+
+ship.exp_supplye()
+mission_traveler()
